c3dfbf4165fda7718c5b68d76ec28a1ecd82ca25
dipak <shanki.dipak@gmail.com>
2025-03-14 22:27:26 +0545

    updated server

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 // API Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/land', landRoutes);
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000", credentials: true })); // Allow frontend requests
 
 // Start Server
 const PORT = process.env.PORT || 5000;
